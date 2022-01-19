55ca5b39a270be96bb7bd51ed136796c13c8fb42
Roland Volskaya <rolandvolskaya@gmail.com>
2022-01-19 14:34:16 +0200

    Fork Flutter's [AnimatedSize]

--- packages/material_switcher/lib/material_switcher.dart
+++ packages/material_switcher/lib/material_switcher.dart
@@ -3,3 +3,6 @@ library material_switcher;
 export 'src/material_image.dart';
 export 'src/material_switcher.dart';
 export 'src/material_view.dart';
+
+export 'src/rendering/animated_size.dart';
+export 'src/widgets/animated_size.dart';
